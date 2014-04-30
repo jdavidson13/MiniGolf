c68b1a929f5e1de8000a94c9f12cf16bd15be645
John D. McLain <tyril132@gmail.com>
2014-04-30 17:39:47 -0400

    Merging JUnit libraries

---
+++ minigolf/TestRound.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package minigolf;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author David
+ *
+ */
+public class TestRound {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
