61ff1f59c0bc7c4e0ed673414da567b0e2c4a547
HubBoi813 <airjays55@yahoo.com>
2014-05-04 21:37:04 -0400

    Update

--- minigolf/CardStationTest.java
+++ minigolf/CardStationTest.java
@@ -1,1 +1,62 @@
- Working on it!!
+ package minigolf;
+import java.io.Console;
+import java.util.ArrayList;
+
+
+/**
+ * @author Josh Duong
+ * @author Jon Davidson
+ * @author Shaz Hosein
+ * @author John McLain
+ */
+
+
+import org.junit.Test;
+
+
+public class CardStation {
+
+	static CardStation;
+
+	private int stationID;
+	private int stationPosition;
+
+
+
+
+}
+
+@Test /* Test to check for user input at card station. */
+	public void testUserInputNotNull() {
+
+
+		System.out.println("\ntestUserInputIsNotNull:");
+		assertNotNull(user);
+		System.out.println("\t" + user + " = passed\n");
+	}
+
+
+@Test /* Test to see if card swipe is valid. */
+	public void testSwipeNotNull() {
+		System.out.println("\ntestSwipeIsValid:");
+		assertTrue(player instanceof Player);
+		System.out.println("\t" + swipe + " = passed\n");
+	}
+
+@Test /* Test to see if Station ID is null. */
+	public void testStationIDIsNotNull() {
+		System.out.println("\ntestStationIDIsNotNull:");
+		assertNotNull(card.getStationID());
+		System.out.println("\t" + card.getStationID() + " = passed\n");
+	}
+
+@Test /* Test to see if Station Position is null. */
+	public void teststationPositionIsNotNull() {
+		System.out.println("\nteststationPositionIsNotNull:");
+		assertNotNull(station.getstationPosition());
+		System.out.println("\t" + station.getPosition() + " = passed\n");
+	}
+
+}
+
+
