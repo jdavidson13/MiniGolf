d0ae4c89e44621c088e6a33c44619f6b3ff29962
tyril132 <tyril132@gmail.com>
2014-05-04 23:10:30 -0400

    Reverting to archived version for submission.

--- minigolf/MainTest.java
+++ minigolf/MainTest.java
@@ -1,44 +1,40 @@
-package minigolftest;
+	
 
-import java.util.ArrayList;
-import static org.junit.Assert.*;
-import org.junit.*;
+    package minigolf;
+     
+    import static org.junit.Assert.*;
+    import java.util.ArrayList;
+    import org.junit.BeforeClass;
+    import org.junit.Test;
+     
+    /**
+     * @author Josh Duong
+     * @author Jon Davidson
+     * @author Shaz Hosein
+     * @author John McLain
+     */
+     
+    public class MainTest {
+           
+            private static ArrayList<Player> players;
+            private static Course c;
+            private static Round r;
+     
+            @BeforeClass
+            public static void setUpBeforeClass() throws Exception {
+                    r = new Main().newRound(players, c);
+            }
+     
+            @Test /* Test to verify that the Round object created by Main.newRound()
+                   * is not null. */
+            public void testNewRoundIsNotNull() {
+                    assertNotNull(r);
+            }
+           
+            @Test /* Test to verify that the Round object created by Main.newRound()
+                   * is a valid Round object. */
+            public void testNewRoundIsCorrectObjectType() {
+                    assertTrue(r instanceof Round);
+            }
+    }
 
-public class MainTest {
-	
-	ArrayList<Player> players;
-	Course sCourse;
-	Round player = new Round(players, sCourse);
-	
-	int playerID = 1;
-	String pName = "";
-	int [] swings = {1 , 2, 3};
-	
-	Player pInfo = new Player(playerID, pName, swings);
-	
-	int legalCourseID = 1;
-    String legalCourseName = "";
-    int legalNumberOfHoles;
-    int[] coursePar = {3, 2, 1};
-   
-    Course c = new Course(legalCourseID, legalCourseName, legalNumberOfHoles, coursePar);
-	
-    @Test
-	public void testNewRound () {
-    	
-    	int r = pInfo.getPlayerStrokes(0);
-    	System.out.println(r);
-		ArrayList<Player> p = player.getPlayers();
-		if(p != null) {
-			System.out.println("Test New Round");
-			equals(p != null);
-			assertEqual(c.getCoursePar(0), pInfo.getPlayerStrokes(0));
-		}
-		
-	}
-
-	private void assertEqual(int coursePar2, int playerStrokes) {
-		// TODO Auto-generated method stub
-		
-	}
-}
